253f39b54a45d2d90c429cdef2d81dfab8eac6bd
smimoon <simonlaroche@orange.fr>
2026-01-29 09:36:03 +0000

    Save end exercice-1

---
+++ exercice-1/src/User.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+const User = (props) => {
+    const [isActive, setIsActive] = useState(false)
+
+    function Connect() {
+        if (isActive === true) {
+            setIsActive(false);
+        }
+        else {
+            setIsActive(true);
+        }
+    }
+    
+    if(isActive === true) {
+        return(
+            <article>
+                <h2>Mari Doucet</h2>
+                <p>En ligne</p>
+                <button onClick={Connect}> Se déconnecter </button>
+            </article>
+        );
+    }
+    else {
+        return (
+            <article>
+                <h2>Mari Doucet</h2>
+                <p>Hors-ligne</p>
+                <button onClick={Connect}> Se connecter </button>
+            </article>
+        );
+    }
+};
+
+export default User;
